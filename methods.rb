d73d7d23fa3626f6dda87cec2a87b3592a9f6656
Jen Tchai <69021958+hopesunknown@users.noreply.github.com>
2023-04-27 18:39:38 -0400

    commit tree d89cec0b5c627376d05cf3e44afb61ca7cd8d997

--- methods.rb
+++ methods.rb
@@ -1,1 +1,41 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+greet "Jimmy"
+greet "Naureen"
+
+
+def greet_with_default (name = "Naureen")
+    puts "Hello, #{name}!"
+end
+
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+
+def add (num1, num2)
+    return num1 + num2
+end
+
+sum1 = add(3, 4)
+
+
+# def halve num1
+#     return num1 / 2
+# end
+
+# sum1 = halve(6)
+
+
+def halve num
+    return nil if num.class != Integer
+
+    num / 2
+end
